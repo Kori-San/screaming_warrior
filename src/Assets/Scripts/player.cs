e21e8deca29611954222e795edae2a6e9b814df8
Tony Blard <tony.blard@epita.fr>
2022-10-19 15:24:21 +0200

    Player health : add maxHealth health and attack porperties. Create method UpdateHealth

--- src/Assets/Scripts/player.cs
+++ src/Assets/Scripts/player.cs
@@ -8,11 +8,32 @@ public class player : MonoBehaviour
     public float speed = .1F;
     private Rigidbody2D rb;
     private Vector3 move;
+    
+    public int maxHp = 100;
+    public int hp = 0;
+    public int attack = 5; 
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+
+        hp = maxHp;
+    }
+
+    void UpdateHealth(int difHp)
+    {
+        hp += difHp;
+        if (hp > maxHp)
+        {
+            hp = maxHp;
+        }
+
+        if (hp < 0)
+        {
+            hp = 0;
+            Debug.Log("Player dead");
+        }
     }
 
     // Update is called once per frame
